5034aba69aabac0684752ab60cda1f9cb5e704d8
madelen <madelen2811@gmail.com>
2022-02-28 11:32:11 -0500

    DATA LOVERS

---
+++ src/data.js
@@ -0,0 +1,14 @@
+// estas funciones son de ejemplo
+
+//export const example = () => {
+ // return 'example';
+//};
+
+// export const anotherExample = () => {
+  //return 'OMG';
+//};
+
+import data from './data/pokemon/pokemon.js';
+
+
+ 
