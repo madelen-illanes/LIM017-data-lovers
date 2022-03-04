6fae1a5eae3cdca0ba1f6c9ee823658cc2ee4bbf
madelen <madelen2811@gmail.com>
2022-03-04 11:59:06 -0500

    accepted changes

--- src/data.js
+++ src/data.js
@@ -3,11 +3,32 @@
 export const example = () => {
   return 'example';
 };
+export const tipoPokemon = (pokemones, str) => {
+  return pokemones.filter(pokemones => pokemones.type == str)}
+
+    /*const grass = pokemones.filter(pokemones => pokemones.type == "grass");
+    const fire = pokemones.filter(pokemones => pokemones.type == "fire" )
+    const water = pokemones.filter(pokemones => pokemones.type == "water" )
+    const electric = pokemones.filter(pokemones => pokemones.type == "electric")
+    const normal = pokemones.filter(pokemones => pokemones.type == "normal")
+    const psychic = pokemones.filter(pokemones => pokemones.type == "psychic")
+    const ice = pokemones.filter(pokemones => pokemones.type == "ice")
+    const ground = pokemones.filter(pokemones => pokemones.type == "ground")
+    const rock = pokemones.filter(pokemones => pokemones.type == "rock")
+    const bug = pokemones.filter(pokemones => pokemones.type == "bug")
+    const steel = pokemones.filter(pokemones => pokemones.type == "steel")
+    const dark = pokemones.filter(pokemones => pokemones.type == "dark")
+    const ghost = pokemones.filter(pokemones => pokemones.type == "ghost")
+    const poison = pokemones.filter(pokemones => pokemones.type == "posion")
+    const fighting = pokemones.filter(pokemones => pokemones.type == "fighting")
+    const land = pokemones.filter(pokemones => pokemones.type == "land)*/
+
+
+
 
 export const anotherExample = () => {
   return 'OMG';
 };
-
 export const showAll = (pokemonInfo) => {
   return pokemonInfo;
 };
--- src/main.js
+++ src/main.js
@@ -1,21 +1,39 @@
 //import { showAll, example, anotherExample } from "./data.js";
 // import data from './data/lol/lol.js';
+import {tipoPokemon} from "./data.js";
 import pokemonInfo from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(pokemonInfo,showAll(pokemonInfo));
 
 
-function mostrarHTML(pokemonInfo) {
-  let pokemones = pokemonInfo.pokemon;
-let contenedor = document.getElementById("pokemones")
-  for (let i = 0; i < pokemones.length; i = i + 1) {
+    const pokemones = pokemonInfo.pokemon;
+    function mostrarHTML(pokemonInfo) {
+    let pokemones = pokemonInfo.pokemon;
+    let contenedor = document.getElementById("pokemones")
+    let contenedor = document.getElementById("pokemones");
+    for (let i = 0; i < pokemones.length; i = i + 1) {
     let pokemon = pokemones[i];
     let imgPokemon = pokemon.img;
     let nodo = document.createElement("img")
     nodo.src=imgPokemon
     contenedor.appendChild(nodo);
 
+    let tagImgPokemon = pokemon.img;
+    let namePokemon = pokemon.name;
+    let divPokemon = document.createElement("div");
+    divPokemon.className = "pokemon-div";
+    let imgPokemon = document.createElement("img");
+    imgPokemon.src = tagImgPokemon;
+    imgPokemon.className= "pokemon-img"
+    let nodoNombre = document.createElement("spam")
+    //nodoNombre.textContent = namePokemon
+    nodoNombre.className = "nombre-item"
+    divPokemon.appendChild(imgPokemon);
+    //divPokemon.appendChild(nodoNombre);
+    contenedor.appendChild(divPokemon);
+    //contenedor.appendChild(nodoNombre);
+    //contenedor.appendChild imgPokemon);
   }
 }
 
@@ -24,3 +42,4 @@ mostrarHTML(pokemonInfo);
 
 
 
+console.log(tipoPokemon(pokemones,"grass"))
