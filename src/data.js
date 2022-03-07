c58537d9c0cfc40998ab4446a39e1f83ef6d624b
CarmenMerino <carmenmerino2202@gmail.com>
2022-03-07 12:08:27 -0500

    todas las funciones

--- src/data.js
+++ src/data.js
@@ -1,6 +1,41 @@
 export const regionPokemon = (pokemones, str) => {
   return pokemones.filter(pokemones=> pokemones.generation.name==str);
 }
+export const typePokemon = (pokemones, str) => {
+  return pokemones.filter(pokemones=> pokemones.type==str);
+}
+
+export const pokemonDescending = (pokemones) => {
+  return pokemones.sort((a,b) => {
+    if (a['name'] < b['name']) {
+      return 1;
+    }
+    if (a['name'] > b['name']) {
+      return -1;
+    }
+    return 0;
+  })
+}
+ 
+export const pokemonUpward = (pokemones) => {
+  return pokemones.sort((a,b) => {
+    if (a['name'] > b['name']) {
+      return 1;
+    }
+    if (a['name'] < b['name']) {
+      return -1;
+    }
+    return 0;
+  })}
+
+
+export const namePokemonFilter = (pokemones, resultPokemon) => {
+  const filteredPokemon = pokemones.filter(pokemones => pokemones.name ===resultPokemon);
+  return filteredPokemon;
+  }
+
+
+
 
 
     /*const grass = pokemones.filter(pokemones => pokemones.type == "grass");
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import {regionPokemon} from "./data.js";
+import {regionPokemon, typePokemon, pokemonDescending, pokemonUpward, namePokemonFilter} from "./data.js";
 import pokemonInfo from "./data/pokemon/pokemon.js";
 
 //console.log(pokemonInfo,showAll(pokemonInfo));
-const pokemones = pokemonInfo.pokemon;
-function mostrarHTML(pokemones) {
-  let contenedor = document.getElementById("pokemones");
+
+const listaPokemones = pokemonInfo.pokemon
+
+function agregarPokemones(pokemones, contenedor) {
   for (let i = 0; i < pokemones.length; i = i + 1) {
     let pokemon = pokemones[i];
     let tagImgPokemon = pokemon.img;
@@ -20,15 +21,78 @@ function mostrarHTML(pokemones) {
     divPokemon.appendChild(imgPokemon);
     divPokemon.appendChild(nodoNombre);
     contenedor.appendChild(divPokemon);
-    //contenedor.appendChild(nodoNombre);
-    //contenedor.appendChild imgPokemon);
   }
 }
-let pokemonKanto = regionPokemon(pokemones,"kanto");
+//HTML
+const contenedor = document.getElementById("pokemones");
+const divRegiones = document.getElementById("region");
+const divKanto = document.getElementById("kanto");
+const divJohto = document.getElementById("johto");
+const btnRegion = document.getElementById("btnRegion");
+const divFire = document.getElementById("typeFire")
+const select = document.getElementById("selection")
+const divAscendente = document.getElementById("ascendente")
+
+agregarPokemones(listaPokemones, contenedor);
+
+let pokemonKanto = regionPokemon(listaPokemones,"kanto");
+let pokemonJohto = regionPokemon(listaPokemones,"johto");
+divRegiones.appendChild(divKanto)
+divRegiones.appendChild(divJohto)
+//agregarPokemones(pokemonJohto, divJohto);
+//agregarPokemones(pokemonKanto, divKanto);
+//divRegiones.appendChild(divKanto)
+//divRegiones.appendChild(divJohto)
+
+function pageRegion() {
+  agregarPokemones(pokemonJohto, divJohto);
+  agregarPokemones(pokemonKanto, divKanto);
+}
+function nextPage(){
+  contenedor.className = "oculto"
+  divRegiones.className = "visible"
+}
 
 
-let pokemonJohto=regionPokemon(pokemones,"johto")
+btnRegion.addEventListener("click", pageRegion)
+btnRegion.addEventListener("click", nextPage)
+
+
+let firePokemon = typePokemon (listaPokemones, "fire")
+//console.log (firePokemon)
+
+function addFire(){
+  agregarPokemones(firePokemon, divFire)
+}
+
+function nextPageFire(){
+  contenedor.className="oculto"
+  divFire.classname="visible"
+}
+// select.addEventListener("input", addFire);
+// select.addEventListener("input", nextPageFire);
+select.addEventListener('change', e => {
+  console.log (typePokemon(listaPokemones, e.target.value))
+})
+
+//orden alfabetico 
+
+const ordenAscendente = pokemonUpward (listaPokemones)
+
+function addAscendete(){
+agregarPokemones (ordenAscendente, divAscendente)
+}
+
+
+
+
+//btnRegion.addEventListener("click", agregarPokemones(pokemonJohto, divRegiones))
+
+
+
+
+
 
 //mostrarHTML(pokemonKanto);
-mostrarHTML(pokemonJohto)
+//mostrarHTML(pokemonJohto)
 
